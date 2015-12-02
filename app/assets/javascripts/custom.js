41223b8a167964378959aab1b99f8251c865acad
Anya Melnyk <gannasachakov@gmail.com>
2015-12-02 11:38:21 -0500

    recipe

---
+++ app/assets/javascripts/custom.js
@@ -0,0 +1,32 @@
+$(function() {
+    console.log("I'm in ");
+
+$('.meal-photo').contenthover({
+    overlay_background:'#000',
+    overlay_opacity:0.8,
+    effect:'slide',
+    slide_speed:500,
+    slide_direction:'left',
+    onshow:function(){
+        $('<div>Hover element shown</div>').appendTo($('#d6_log'));
+    },
+    onhide:function(){
+        $('<div>Hover element hidden</div>').appendTo($('#d6_log'));
+    }
+});
+
+
+
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+
+
+
+});
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
 
   root 'welcome#index'
+  resources :recipes
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
